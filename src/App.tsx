40f0a7b3c60af45db28cfb097f4022d2ea139773
Timmy Jose <zoltan.jose@gmail.com>
2024-03-10 17:04:44 +0530

    Address details and balance loaded successfully in Connect.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,31 @@ import * as React from 'react'
 import Home from './components/Home'
 import Connect from './components/Connect'
 import Calculator from './components/Calculator'
+import { mainnet, polygon, arbitrum, sepolia } from 'viem/chains'
+import { Web3Modal, createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi-react-native'
+import { WagmiConfig } from 'wagmi'
+
+const projectId = 'd354077797a1b871f3e92f0f58ec2549'
+
+const metadata = {
+  name: 'wallet-signed-calculator',
+  description: 'Wallet Signed Calculator App',
+  url: 'https://github.com/timmyjose-web3/wallet-signed-calculator',
+  icons: [],
+  redirect: {
+    native: '',
+    universal: ''
+  }
+}
+
+const chains = [mainnet, polygon, arbitrum, sepolia]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata})
+
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig
+})
 
 export type RootParamsList = {
   Home: undefined
@@ -15,12 +40,15 @@ const Stack = createNativeStackNavigator()
 
 export default function App () {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={Home} />
-        <Stack.Screen name='Connect' component={Connect} />
-        <Stack.Screen name='Calculator' component={Calculator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <WagmiConfig config={wagmiConfig}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={Home} />
+          <Stack.Screen name='Connect' component={Connect} />
+          <Stack.Screen name='Calculator' component={Calculator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Web3Modal />
+    </WagmiConfig>
   )
 }
--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -3,12 +3,30 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import * as React from 'react'
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 import { RootParamsList } from '../App'
+import { useWeb3Modal } from '@web3modal/wagmi-react-native'
+import { useAccount, useBalance } from 'wagmi'
 
 export default function Connect() {
   const navigation = useNavigation<NativeStackNavigationProp<RootParamsList>>()
 
+  const { open } = useWeb3Modal()
+
+  const { address, isConnecting, isDisconnected } = useAccount()
+  const { data, isError: isErrorBalance, isLoading: isLoadingBalance } = address ? useBalance({
+    address
+  }) : { data: null, isError: false, isLoading: true}
+
   return (
     <View style={styles.container}>
+      <Pressable style={styles.button} onPress={() => open()}>
+        <Text style={styles.buttonText}>Connect</Text>
+      </Pressable>
+      {isConnecting && (<Text>Address Loading...</Text>)}
+      {isDisconnected && (<Text>Not connected</Text>)}
+      {!isConnecting && !isDisconnected && (<Text>Address: {address}</Text>)}
+      {isLoadingBalance && (<Text>Balance loading...</Text>)}
+      {isErrorBalance && (<Text>Could not retrieve balance</Text>)}
+      {!isLoadingBalance && !isErrorBalance && (<Text>Balance: {data?.formatted}{data?.symbol}</Text>)}
       <Pressable style={styles.button} onPress={() => navigation.goBack()}>
         <Text style={styles.buttonText}>Go Back</Text>
       </Pressable>
